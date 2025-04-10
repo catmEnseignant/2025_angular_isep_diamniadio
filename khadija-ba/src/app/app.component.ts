68af2bcf0e65628db11a99c8bb720fab2e4786e9
KhadidiatouBa <k.ba5_1585@isepdiamniadio.edu.sn>
2025-04-10 14:50:59 +0000

    Ajout de deux composants classe et matiere dans le composant app

--- khadija-ba/src/app/app.component.ts
+++ khadija-ba/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
+import { ClasseComponent } from './classe/classe.component';
+import { MatiereComponent } from './matiere/matiere.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CoursesComponent],
+  imports: [RouterOutlet,ClasseComponent,MatiereComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MyDemoApp';
+  title = 'khadija-ba';
 }
