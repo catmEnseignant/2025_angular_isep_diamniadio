b7fa7fa58e80dd696cb3fa4285f5069a510c2143
fatou bob <fatoubob609@gmail.com>
2025-04-10 15:58:16 +0100

    projet avec les classer merci

--- fatou_bob/src/app/app.routes.ts
+++ fatou_bob/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { ClasseComponent } from './classe/classe.component';
+import { MatiereComponent } from './matiere/matiere.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"liste-classes",component:ClasseComponent},
+    {path:"liste-matieres",component:MatiereComponent},
+    {path:"",component:ClasseComponent},
+];
