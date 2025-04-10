b543fd9e91094f2513fa28c9de57a3c52cd4b890
asta <asta60742@hmail.com>
2025-04-10 16:05:05 +0000

    Ajout de deux composants classe et matieres

--- asta_mbacke/src/app/app.routes.ts
+++ asta_mbacke/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ClasseComponent } from './classe/classe.component';
+import { MatieresComponent } from './matieres/matieres.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"liste-Classe", component:ClasseComponent},
+
+    {path:"liste-Matiere", component:MatieresComponent},
+
+    {path:"", component:ClasseComponent},
+];
