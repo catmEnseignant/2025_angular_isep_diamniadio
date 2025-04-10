49bd62674e074b28b03e561c31959161b52a6895
Aida <tine91@gmail.com>
2025-04-10 15:56:05 +0100

    Ajout de classes et matieres

--- Aida-Bouna-Tine/src/app/app.component.ts
+++ Aida-Bouna-Tine/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { ClasseComponent } from './classe/classe.component';
 import { MatiereComponent } from './matiere/matiere.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ClasseComponent,MatiereComponent],
+  imports: [RouterModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Aida-Bouna-Tine/src/app/app.routes.ts
+++ Aida-Bouna-Tine/src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import path from 'path';
+import { ClasseComponent } from './classe/classe.component';
+import { MatiereComponent } from './matiere/matiere.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"list-classes",component:ClasseComponent},
+    {path:"list-matieres",component:MatiereComponent},
+    {path:"",component:ClasseComponent},
+
+];
