bbff9a1b712ba5cdde66819ab391f9f94ae21ba6
DIA <penda.gueda@gmail.com>
2025-04-10 17:56:13 +0200

    Routage des Compsants

--- penda_dia/src/app/app.routes.ts
+++ penda_dia/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ClasseComponent } from './classe/classe.component';
+import { MatieresComponent } from './matieres/matieres.component';
+
+//creer des router et les importer
+export const routes: Routes = [
+    {path:"liste-classes",component:ClasseComponent},
+    {path:"liste-matieres",component:MatieresComponent},
+    {path:"",component:ClasseComponent}
+];
