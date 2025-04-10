ae71047e34caf13df3ff5151b848c4fdbb27ab21
Alves <mbao221boye@gmail.com>
2025-04-10 22:59:43 +0100

    Ajout de deux composants

--- mbao_boye/src/app/app.routes.ts
+++ mbao_boye/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ClasseComponent } from './classe/classe.component';
+import { MatieresComponent } from './matieres/matieres.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"liste-classes",component:ClasseComponent},
+  {path:"liste-matieres",component:MatieresComponent},
+  {path:"",component:ClasseComponent},
+];
