148d293bd1a33717b919e96c43ee88c10e851c85
Lamine Kama <laminekama038@gmail.com>
2025-04-14 19:07:57 +0800

    ajout des routes

--- lamine-kama/src/app/app.routes.ts
+++ lamine-kama/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ClassesComponent } from './classes/classes.component';
+import { MatieresComponent } from './matieres/matieres.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'matieres', component: MatieresComponent},
+    {path: 'classes', component: ClassesComponent},
+    {path: '', component: ClassesComponent},
+];
