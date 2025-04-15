b04917cedd3f366ad5317fffdb68a7eeb07e2634
alettendongo <alettendongo@icloud.com>
2025-04-15 10:18:37 +0200

    Ajout et modification des composants et des routes

--- alette_ndongo/src/app/app.routes.ts
+++ alette_ndongo/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ClasseComponent } from './classe/classe.component';
+import { MatieresComponent } from './matieres/matieres.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"Liste-classes",component:ClasseComponent},
+    {path:"Liste-matieres",component:MatieresComponent},
+    {path:"",component:ClasseComponent},
+];
