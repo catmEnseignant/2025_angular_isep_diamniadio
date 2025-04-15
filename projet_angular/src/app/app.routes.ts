679e8ae7816f2923eec58b135d1fb4a7d573fa41
aissatatalldiop <aissatatalldiop67@gmlai.com>
2025-04-15 09:24:44 +0000

    Ajout de bootstrap

--- projet_angular/src/app/app.routes.ts
+++ projet_angular/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ClasseComponent } from './classe/classe.component';
+import { MatiereComponent } from './matiere/matiere.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:"list-classes",component:ClasseComponent},
+    {path:"list-matieres",component:MatiereComponent},
+    {path:"",component:ClasseComponent},
+];
