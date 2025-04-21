de52af222f85e716828d13d081f77d443e14f75e
Rokhaya <diofrakhy@gmail.com>
2025-04-21 18:28:49 +0000

    initial commit

--- Rokhaya-diouf/src/app/app.routes.ts
+++ Rokhaya-diouf/src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 import { ClasseComponent } from './classe/classe.component';
 import { MatiereComponent } from './matiere/matiere.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [
-    {path:"liste-classes",component:ClasseComponent},
-    {path:"liste-matieres",component:MatiereComponent},
-    {path:"",component:ClasseComponent},
+
+export const APP_ROUTES: Routes = [
+    {path:"classes",
+        loadChildren:() => import("./classes/classes.routes").then((m) => m.CLASSE_ROUTES),
+    },
+
+    {path:"",component:HomeComponent},
+
+
 ];
