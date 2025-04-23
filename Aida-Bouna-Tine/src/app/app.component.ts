f4580b5d8553e2e35de7829101d9069e99307912
Aida <tine91@gmail.com>
2025-04-24 00:21:07 +0100

    feat: correction pour fixer le footer en bas avec un layout flex CSS

--- Aida-Bouna-Tine/src/app/app.component.ts
+++ Aida-Bouna-Tine/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { ClasseComponent } from './classe/classe.component';
 import { MatiereComponent } from './matiere/matiere.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule,RouterOutlet],
+  imports: [RouterModule,RouterOutlet,NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Aida-Bouna-Tine/src/app/app.routes.ts
+++ Aida-Bouna-Tine/src/app/app.routes.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { Routes } from '@angular/router';
-import path from 'path';
-import { ClasseComponent } from './classe/classe.component';
-import { MatiereComponent } from './matiere/matiere.component';
-import { AppComponent } from './app.component';
 
-export const routes: Routes = [
-    {path:"list-classes",component:ClasseComponent},
-    {path:"list-matieres",component:MatiereComponent},
-    {path:"",component:ClasseComponent},
+import { Routes } from '@angular/router';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
+export const APP_ROUTES: Routes = [
+    {path:"classes",
+        loadChildren:() => import("./classes/classes.routes").then((m)=> m.CLASSE_ROUTES),
+    },
+   
+    {path:"",component:HomeComponent},
+
+
+    {path:"matieres",
+        loadChildren:() => import("./matieres/matieres.routes").then((m)=> m.MATIERE_ROUTES),
+    },
+   
+    {path:"",component:HomeComponent},
 
 ];
+
+
