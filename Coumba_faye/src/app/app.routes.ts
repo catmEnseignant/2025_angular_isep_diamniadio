78eda5737f7397d5e762bde788bb06c13a83ae66
Coumba faye <c.faye5@isepat.edu.sn>
2025-04-23 02:46:16 +0200

    creation de nouvelles composants

--- Coumba_faye/src/app/app.routes.ts
+++ Coumba_faye/src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 import { ClasseComponent } from './classe/classe.component';
 import { MatiereComponent } from './matiere/matiere.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [
-    {path:"liste-classes",component:ClasseComponent},
-    {path:"liste-matieres",component:MatiereComponent},
-    {path:"",component:ClasseComponent},
+export const APP_ROUTES: Routes = [
+    {path:"",component:HomeComponent},
+    
+    {path:"classes", loadChildren:() => import("./classes/classes.routes").then((m) => m.CLASSES_ROUTES)},    
+    
+    {path:"matieres", loadChildren:() => import("./matieres/matieres.routes").then((m) => m.MATIERES_ROUTES)},    
+    
+   
+
+
 ];
---
+++ Coumba_faye/src/app/classes/classes.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { ClasseComponent } from '../classe/classe.component';
+
+export const CLASSES_ROUTES: Routes = [
+    {path:"list-classe",component:ClasseComponent},
+    {path:"form-classe",component:ClasseComponent},
+    {path:"",component:ClasseComponent},
+];
