599c9fcbff2ccbb95bab20a80ec2b6a136084eb2
KhadidiatouBa <k.ba5_1585@isepdiamniadio.edu.sn>
2025-04-24 09:40:38 +0000

    componnent
    "ajout liste et form dans componnent"

--- khadija-ba/src/app/app.component.ts
+++ khadija-ba/src/app/app.component.ts
@@ -3,10 +3,13 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 import { ClasseComponent } from './classe/classe.component';
 import { MatiereComponent } from './matiere/matiere.component';
+import { NavebarComponent } from './shared/navebar/navebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule,RouterOutlet],
+  imports: [RouterModule,RouterOutlet,NavebarComponent,FooterComponent],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ khadija-ba/src/app/shared/navebar/navebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navebar',
+  imports: [],
+  templateUrl: './navebar.component.html',
+  styleUrl: './navebar.component.css'
+})
+export class NavebarComponent {
+
+}
