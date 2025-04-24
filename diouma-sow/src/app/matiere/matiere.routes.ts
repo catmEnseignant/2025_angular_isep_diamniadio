b72fea73da9ab5d522d9ea8c1fcedc241bfaef2a
diouma <dioumasow0601@gmail.com>
2025-04-24 10:02:50 +0000

    initialisation du projet

--- diouma-sow/src/app/matiere/matiere.routes.ts
+++ diouma-sow/src/app/matiere/matiere.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { MatieresComponent } from '../matieres/matieres.component';
+import { ListMatiereComponent } from './list-matiere/list-matiere.component';
+import { FormMatiereComponent } from './form-matiere/form-matiere.component';
 
 
 export const MATIERE_ROUTES: Routes = [
-  {path:"liste-matieres",component:MatieresComponent},
-  {path:"form-matiere",component:MatieresComponent},
+  {path:"liste-matieres",component:ListMatiereComponent},
+  {path:"form-matiere",component:FormMatiereComponent},
   {path:"",component:MatieresComponent},
 
 ];
