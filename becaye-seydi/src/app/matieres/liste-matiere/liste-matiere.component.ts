aafe767f86fe700238bf3c1e2be51428826c2097
Becaye seydi <becayeseydi265@email.com>
2025-04-29 09:59:19 +0000

    Premier commit après configuration

--- becaye-seydi/src/app/matieres/liste-matiere/liste-matiere.component.ts
+++ becaye-seydi/src/app/matieres/liste-matiere/liste-matiere.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-liste-matiere',
@@ -7,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './liste-matiere.component.css'
 })
 export class ListeMatiereComponent {
+  constructor(private route:Router){
+    console.log("constructeur")
+  }
+  addClasse(){
+    return this.route.navigate(["matieres/form-matiere"])
+
+  }
 
 }
--- becaye-seydi/src/app/matieres/matieres.routes.ts
+++ becaye-seydi/src/app/matieres/matieres.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
 import { MatiereComponent } from '../matiere/matiere.component';
 import { AppComponent } from '../app.component';
+import { ListClasseComponent } from '../classes/list-classe/list-classe.component';
+import { FormClasseComponent } from '../classes/form-classe/form-classe.component';
 
 
 
 export const MATIERES_ROUTES : Routes = [
-    {path:"liste-matiere",component:MatiereComponent},
-    {path:"form-matiere",component:MatiereComponent},
-
-
+    {path:"liste-matiere",component:ListClasseComponent},
+    {path:"form-matiere",component:FormClasseComponent},
     {path:"",redirectTo:"/liste-matiere",pathMatch:"full"}
-   
+    
 
 
 ];
