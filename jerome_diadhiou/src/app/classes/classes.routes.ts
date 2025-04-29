643181cc0188efe38a7acf5730536fe2b55d6547
jeromeland <jeromediadhiou1@gmail.com>
2025-04-29 09:01:00 +0000

    formulaire

--- jerome_diadhiou/src/app/classes/classes.routes.ts
+++ jerome_diadhiou/src/app/classes/classes.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
-import { ClasseComponent } from '../classe/classe.component';
+import { ListClasseComponent } from './list-classe/list-classe.component';
+import { FormClasseComponent } from './form-class/form-class.component';
 
 
 export const CLASSES_ROUTES: Routes = [
-  {path:"list-classes",component:ClasseComponent},
-  {path:"form-classes",component:ClasseComponent},
-  {path:"",component:ClasseComponent},
+  {path:"list-classes",component:ListClasseComponent},
+  {path:"form-classes",component:FormClasseComponent},
+  {path:"",redirectTo:"/list-classes",pathMatch:"full"},
 
 ];
 
--- jerome_diadhiou/src/app/classes/form-class/form-class.component.spec.ts
+++ jerome_diadhiou/src/app/classes/form-class/form-class.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FormClasseComponent } from './form-classe.component';
+import { FormClasseComponent } from './form-class.component';
 
 describe('FormClasseComponent', () => {
   let component: FormClasseComponent;
